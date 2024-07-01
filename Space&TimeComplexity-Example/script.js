a4eb1e3bd88a49334187d9b7d47ab27a50ccd08b
Noman-Nom <noman5456318@gmail.com>
2024-07-01 16:43:27 +0500

    big 0 example

--- Space&TimeComplexity-Example/script.js
+++ Space&TimeComplexity-Example/script.js
@@ -1,3 +1,20 @@
 const arr = [0 ,1 ,2 ,3 , 4 , 5 ,6 , 7 , 8 , 9 , 10 ]
 //  big-0 (1)  or constant - 0(1)
-console.log(arr[0])
+// console.log(arr[0])
+
+
+
+//  space and time complexity example of constants 
+
+// Time Complexity: O(1)
+
+// An algorithm has constant time complexity when the time it takes to execute is independent of the size of the input data. This means that no matter how large the input is, the execution time remains the same.
+
+  const arr1 = [0 ,1 ,20 ,3 , 4 , 5 ,6 , 7 , 8 , 9 , 10 ]
+
+  function CheckTimeComplexity(array , index){
+
+    return array[index]
+  }
+
+  console.log(CheckTimeComplexity(arr1 , 2));
