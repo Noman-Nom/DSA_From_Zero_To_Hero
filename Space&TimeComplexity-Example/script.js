82d7b47578be4d19d5e4a0f637456aacc1fbb2a9
Noman-Nom <noman5456318@gmail.com>
2024-07-01 16:54:43 +0500

    big 0 example

--- Space&TimeComplexity-Example/script.js
+++ Space&TimeComplexity-Example/script.js
@@ -10,6 +10,7 @@ const arr = [0 ,1 ,2 ,3 , 4 , 5 ,6 , 7 , 8 , 9 , 10 ]
 
 // An algorithm has constant time complexity when the time it takes to execute is independent of the size of the input data. This means that no matter how large the input is, the execution time remains the same.
 
+// example 1 
   const arr1 = [0 ,1 ,20 ,3 , 4 , 5 ,6 , 7 , 8 , 9 , 10 ]
 
   function CheckTimeComplexity(array , index){
@@ -18,3 +19,24 @@ const arr = [0 ,1 ,2 ,3 , 4 , 5 ,6 , 7 , 8 , 9 , 10 ]
   }
 
   console.log(CheckTimeComplexity(arr1 , 2));
+
+
+//    Space Complexity --  0(1)
+
+// An algorithm has constant space complexity when the amount of memory it requires does not change with the size of the input data. This means it uses a fixed amount of space regardless of the input size.
+
+
+
+const arr2 = [0, 1 , 2 ,3 ,4 , 5]
+console.log( "length of array ",arr2.length - 1)
+
+
+// example 3  
+
+const arr3 = [0, 1 , 2 ,3 ,4 , 5]
+
+let num = 10 
+
+arr3[3] = arr3[3] + num
+
+console.log(arr3[3]);
