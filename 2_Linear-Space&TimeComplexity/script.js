26887dbaddb2d587799222cb6dc26beef4feec73
Noman-Nom <noman5456318@gmail.com>
2024-07-02 15:53:01 +0500

    chnages

--- 2_Linear-Space&TimeComplexity/script.js
+++ 2_Linear-Space&TimeComplexity/script.js
@@ -15,4 +15,20 @@ for (let i = 0; i < arr1.length -1 ; i++) {
 
 // example 2  Copying array 
 
-const arr2 = [ 1, 2 ,3 ,4 ,5 , 6 , 7, 8, 9, 10]
+const arr2 = [ 1, 2 ,3 ,4 ,5 , 6 , 7, 8, 9]
+
+const copyArrayFunction = ()=>{
+
+
+    let  newArray = []
+    for (let i = 0; i < arr2.length; i++) {
+        newArray.push(arr2[i])
+
+        
+    }
+    return newArray
+}
+
+      copyArray  = copyArrayFunction(arr2)
+      console.log("Array = ", arr2)
+      console.log("Copy Array =", copyArray )
