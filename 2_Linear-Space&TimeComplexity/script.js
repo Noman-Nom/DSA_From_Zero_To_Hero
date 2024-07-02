8c655f1e0e856bb24e83b026d8d8ea4d33d5007c
Noman-Nom <noman5456318@gmail.com>
2024-07-02 15:28:51 +0500

    changes

---
+++ 2_Linear-Space&TimeComplexity/script.js
@@ -0,0 +1,18 @@
+// Linear Space Complexity: O(n)
+
+// An algorithm has linear space complexity when the amount of memory it requires is directly proportional to the size of the input data.
+
+//  example 1 
+
+const arr1 = [ 1, 2 ,3 ,4 ,5 , 6 , 7, 8]
+
+for (let i = 0; i < arr1.length -1 ; i++) {
+    const element = arr1[i];
+
+    // console.log(element);
+    
+}
+
+// example 2  Copying array 
+
+const arr2 = [ 1, 2 ,3 ,4 ,5 , 6 , 7, 8, 9, 10]
