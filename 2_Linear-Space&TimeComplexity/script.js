4e240254443186a1d432d0b5af6499d71c5e94e5
Noman-Nom <noman5456318@gmail.com>
2024-07-02 16:01:39 +0500

    chnages

--- 2_Linear-Space&TimeComplexity/script.js
+++ 2_Linear-Space&TimeComplexity/script.js
@@ -13,7 +13,11 @@ for (let i = 0; i < arr1.length -1 ; i++) {
     
 }
 
-// example 2  Copying array 
+
+
+// example 2  Copying array   ------->   The amount of memory used by newArray is directly proportional to the size of the input array.
+
+// Therefore, the space complexity is O(n)
 
 const arr2 = [ 1, 2 ,3 ,4 ,5 , 6 , 7, 8, 9]
 
@@ -30,5 +34,29 @@ const copyArrayFunction = ()=>{
 }
 
       copyArray  = copyArrayFunction(arr2)
-      console.log("Array = ", arr2)
-      console.log("Copy Array =", copyArray )
+    //   console.log("Array = ", arr2)
+    //   console.log("Copy Array =", copyArray )
+
+
+    //   example 3 Sum of Elements in an Array  ---->The number of operations increases linearly with the number of elements in the array.
+    // Therefore, the time complexity is O(n).
+
+
+            const arr3 = [ 1, 2 ,3 ,4 ,5 , 6 , 7, 8, 9]
+
+            const sumArrayFunction = (arr3)=>{
+
+                let sum = 0
+
+                for (let i = 0; i < arr3.length; i++) {
+                    sum = sum + arr3[i]
+
+
+                    
+                }
+                return sum
+            }
+
+            sumOfArray = sumArrayFunction(arr3)
+            console.log("Array = ", arr3)
+            console.log("Sum of array = ", sumOfArray)
