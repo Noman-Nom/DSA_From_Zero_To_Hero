e5e7763340e4fb134e052367bfd85fa2fa3b96b8
Noman-Nom <noman5456318@gmail.com>
2024-07-09 16:40:04 +0500

    changes

---
+++ 3_Logarithmic_Time&Space_Complexity/Script.js
@@ -0,0 +1,25 @@
+//  recall while 
+
+let count = 0 
+
+while(count<2){
+    console.log("Count is :", count)
+    count ++ ;
+}
+
+
+let number = 1
+let sum = 0 
+
+while(number<=10){
+    sum += number 
+    number ++
+
+}
+console.log("sum = ", sum)
+
+
+// Example Problems
+// Problem 1: Binary Search
+// Given a sorted array [1, 3, 4, 5, 6, 8, 9] and a target value 6, implement a binary search to find the index of 6.
+
