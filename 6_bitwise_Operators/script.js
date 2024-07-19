04a2f5123ce10c657242e5d92239040de3b615e6
Noman-Nom <noman5456318@gmail.com>
2024-07-19 16:32:09 +0500

    bitwise operator

--- 6_bitwise_Operators/script.js
+++ 6_bitwise_Operators/script.js
@@ -1,13 +1,13 @@
                 //  program without bitwise operators 
 
                 //  check number is Even ier ODD using decimal 
-                 const num =4 
+                 const num =9
                  if (num %2 ===0) {
-                    console.log("Number is even")
+                    // console.log("Number is even")
                     
                  }
                  else{
-                    console.log("Number is odd")
+                    // console.log("Number is odd")
                  }
 
             //  Bitwise operators
@@ -15,10 +15,22 @@
             let a = 5;   // 0101 in binary
             let b = 3;   // 0011 in binary
             
-            console.log(a & b);  // Output: 1 (0001 in binary)
-            console.log(a | b);  // Output: 7 (0111 in binary)
-            console.log(a ^ b);  // Output: 6 (0110 in binary)
-            console.log(~a);     // Output: -6 (inverts the bits of 5)
-            console.log(a << 1); // Output: 10 (1010 in binary)
-            console.log(a >> 1); // Output: 2 (0010 in binary)
-            console.log(a >>> 1);// Output: 2 (0010 in binary)
+            // console.log(a & b);  // Output: 1 (0001 in binary)
+            // console.log(a | b);  // Output: 7 (0111 in binary)
+            // console.log(a ^ b);  // Output: 6 (0110 in binary)
+            // console.log(~a);     // Output: -6 (inverts the bits of 5)
+            // console.log(a << 1); // Output: 10 (1010 in binary)
+            // console.log(a >> 1); // Output: 2 (0010 in binary)
+            // console.log(a >>> 1);// Output: 2 (0010 in binary)
+
+
+
+            //  check number is even or odd  using bitwise operator 
+
+            if((num & 1) === 0){
+                console.log("Number is EVEN")
+            }
+            else{
+                console.log("NUmber is ODD")
+            }
+
