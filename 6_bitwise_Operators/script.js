f22d996c91c7bb7ddf2aa3c1e071b8d8a2387466
Noman-Nom <noman5456318@gmail.com>
2024-07-19 16:10:02 +0500

    bitwise operator

---
+++ 6_bitwise_Operators/script.js
@@ -0,0 +1,13 @@
+                //  program without bitwise operators 
+
+                //  check number is Even ier ODD using decimal 
+                 const num =4 
+                 if (num %2 ===0) {
+                    console.log("Number is even")
+                    
+                 }
+                 else{
+                    console.log("Number is odd")
+                 }
+
+            
