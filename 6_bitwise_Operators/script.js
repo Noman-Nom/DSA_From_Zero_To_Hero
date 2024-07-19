5cf82f591158c5c0026258440935eb4f7dc42c3e
Noman-Nom <noman5456318@gmail.com>
2024-07-19 16:19:33 +0500

    bitwise operator

--- 6_bitwise_Operators/script.js
+++ 6_bitwise_Operators/script.js
@@ -10,4 +10,15 @@
                     console.log("Number is odd")
                  }
 
+            //  Bitwise operators
+
+            let a = 5;   // 0101 in binary
+            let b = 3;   // 0011 in binary
             
+            console.log(a & b);  // Output: 1 (0001 in binary)
+            console.log(a | b);  // Output: 7 (0111 in binary)
+            console.log(a ^ b);  // Output: 6 (0110 in binary)
+            console.log(~a);     // Output: -6 (inverts the bits of 5)
+            console.log(a << 1); // Output: 10 (1010 in binary)
+            console.log(a >> 1); // Output: 2 (0010 in binary)
+            console.log(a >>> 1);// Output: 2 (0010 in binary)
